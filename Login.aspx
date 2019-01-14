0658d064b7c41608689e2e5ce6177ad029614a9f
ZarishYosaf <45290043+ZarishYosaf@users.noreply.github.com>
2019-01-14 06:32:26 -0800

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="Log in" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="FinalProject.Account.Login" %>
+
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+
+        <div class="box">
+            <h2 style="font-family:Helvetica">Login</h2>
+            <div class="inputbox">
+                <asp:TextBox ID="txtUserName" runat="server" required=""></asp:TextBox>
+                <asp:Label ID="lblUserName" runat="server" Text="UserName" CssClass="label"></asp:Label>
+            </div>
+            <div class="inputbox">
+                <asp:TextBox ID="txtPassword" runat="server" required="" TextMode="Password"></asp:TextBox>
+                <asp:Label ID="lblPassword" runat="server" Text="Password" CssClass="label"></asp:Label>
+            </div>
+          
+           <div style="float:left; font-size:14px; color:white; margin-top:1%">
+               <asp:CheckBox ID="CheckBox1" runat="server" />Remember Me</div>
+               <div style="float:right; color:white; font-size:14px; margin-top:3%">
+               <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/Account/ChangePassword.aspx" ForeColor="White" Font-Underline="False">Forgot Password?</asp:HyperLink>
+           </div>
+            <div style="margin-top:8%">
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click"  /></div>
+
+          
+        </div>
+     
+     
+    
+
+    
+</asp:Content>
---
+++ Login.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using BLL;
+using DAL;
+
+
+namespace FinalProject.Account
+{
+    public partial class Login : Page
+    {
+        UserBLL objUserBLL = new UserBLL();
+        UserDAL objUserDAL = new UserDAL();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Session["UserID"] = null;
+            Session["UserType"] = null;
+            Session["UserName"] = null;
+            
+        }
+        public void FillBLL()
+        {
+            objUserBLL.UserName = txtUserName.Text.Trim();
+            objUserBLL.Password = txtPassword.Text.Trim();
+            objUserBLL.UserType = Request.Form.Get("UserType");
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            FillBLL();
+            DataTable dt = new DataTable();
+         
+
+            dt=objUserDAL.GetUsers(objUserBLL);
+        
+
+            if (dt.Rows.Count >= 0)
+            {
+                string UserType = dt.Rows[0]["UserType"].ToString();
+                string UserID = dt.Rows[0]["UserID"].ToString();
+                if (UserType=="Admin")
+                {
+                    Session["UserID"] = UserID;
+                    Session["UserType"] = UserType;
+                    Session["UserName"] = objUserBLL.UserName;
+                    Response.Redirect("../admin/Dashboard_HomePage.aspx");   
+                }
+                else if (UserType=="Supervisor")
+                {
+                    Session["UserID"] = UserID;
+                    Session["UserType"] = UserType;
+                    Session["UserName"] = objUserBLL.UserName;
+                    Response.Redirect("../Supervisor/MainSupervisor.aspx");
+                }
+                else if (UserType == "Second Marker")
+                {
+                    Session["UserID"] = UserID;
+                    Session["UserType"] = UserType;
+                    Session["UserName"] = objUserBLL.UserName;
+                    Response.Redirect("../SecondMarker/MainSecondMarker.aspx");
+                }
+                else if (UserType == "Student")
+                {
+                    Session["UserID"] = UserID;
+                    Session["UserType"] = UserType;
+                    Session["UserName"] = objUserBLL.UserName;
+                    Response.Redirect("../Student/MainStudent.aspx");
+                    
+                }
+            }
+            else {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Invalid UserName!')", true);
+            }
+
+
+        }
+    }
+}
